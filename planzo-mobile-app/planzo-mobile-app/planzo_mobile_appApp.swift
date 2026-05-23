9e80e6bf3c6d4bab82b788a060c5f7833e99a636
mpearson-11 <max.pearson1@googlemail.com>
2026-05-23 09:30:05 +0100

    init

---
+++ planzo-mobile-app/planzo-mobile-app/planzo_mobile_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  planzo_mobile_appApp.swift
+//  planzo-mobile-app
+//
+//  Created by Max Pearson on 23/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct planzo_mobile_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ planzo-mobile-app/planzo-mobile-appTests/planzo_mobile_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  planzo_mobile_appTests.swift
+//  planzo-mobile-appTests
+//
+//  Created by Max Pearson on 23/05/2026.
+//
+
+import Testing
+@testable import planzo_mobile_app
+
+struct planzo_mobile_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
